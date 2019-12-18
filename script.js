860537b716cac9f9a672e512851c86fb5e3f9d50
Saif <saifraikkonen@gmail.com>
2019-12-18 23:16:29 +0530

    player functionality fix

--- script.js
+++ script.js
@@ -1,18 +1,45 @@
 let gameCanvas = document.getElementById("gameCanvas");
 let gameContext = gameCanvas.getContext("2d");
+let xTurn = true;
+let blocks = Array(9).fill(false);
+let gameOver = false;
 
-const ticTacColumns = [
+const zone = [
   {
-    x1: 0,
-    y1: 0,
-    x2: 200,
-    y2: 200
+    x: 0,
+    y: 0
   },
   {
-    x1: 200,
-    y1: 0,
-    x2: 400,
-    y2: 200
+    x: 200,
+    y: 0
+  },
+  {
+    x: 400,
+    y: 0
+  },
+  {
+    x: 0,
+    y: 200
+  },
+  {
+    x: 200,
+    y: 200
+  },
+  {
+    x: 400,
+    y: 200
+  },
+  {
+    x: 0,
+    y: 400
+  },
+  {
+    x: 200,
+    y: 400
+  },
+  {
+    x: 400,
+    y: 400
   }
 ];
 
@@ -38,24 +65,33 @@ function drawGame() {
   gameContext.stroke();
 }
 
-function startGame(event) {
-  drawGame();
+function process(event) {
+  if (!gameOver) {
+    let select = getUserSelection(event);
+    let x = select.x;
+    let y = select.y;
+    let block = getBlockNumber(x, y);
 
-  let xTurn = true;
-  let blocks = Array(9).fill(false);
-  do {
-    const { x, y } = getUserSelection(event);
+    if (isBlockAvailable(blocks, block)) {
+      drawXO(xTurn, block);
+      blocks[block] = true;
+      xTurn = !xTurn;
+      if (xTurn) document.getElementById("status").innerHTML = "Player X turn";
+      else document.getElementById("status").innerHTML = "Player O turn";
+    } else {
+      document.getElementById("status").innerHTML =
+        "Cannot redraw here. Pick an empty block.";
+    }
 
-    let block;
-    do {
-      block = getBlockNumber(x, y);
-    } while (isBlockAvailable(blocks, block));
+    let allTrue = !!blocks.reduce(function(a, b) {
+      return a === b ? a : NaN;
+    });
 
-    blocks[block] = true;
-    drawXO(xTurn, block);
-
-    xTurn = !xTurn;
-  } while (blocks.indexOf(false) !== -1);
+    if (allTrue) {
+      document.getElementById("status").innerHTML = "Game over";
+      gameOver = true;
+    }
+  }
 }
 
 function getUserSelection(event) {
@@ -84,13 +120,33 @@ function isBlockAvailable(blocks, block) {
 }
 
 function drawXO(xTurn, block) {
-  if (xTrue) {
-    console.log(" x turn ");
+  if (xTurn) {
+    gameContext.beginPath();
+    gameContext.moveTo(zone[block].x, zone[block].y);
+    gameContext.lineTo(zone[block].x + 200, zone[block].y + 200);
+    gameContext.lineWidth = 5;
+    gameContext.strokeStyle = "#ffa500";
+    gameContext.stroke();
+
+    gameContext.beginPath();
+    gameContext.moveTo(zone[block].x + 200, zone[block].y);
+    gameContext.lineTo(zone[block].x, zone[block].y + 200);
+    gameContext.lineWidth = 5;
+    gameContext.strokeStyle = "#ffa500";
+    gameContext.stroke();
   } else {
-    console.log(" o turn ");
+    let centerX = zone[block].x + 100,
+      centerY = zone[block].y + 100,
+      radius = 100;
+    gameContext.beginPath();
+    gameContext.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+    gameContext.fillStyle = "green";
+    gameContext.fill();
+    gameContext.lineWidth = 2;
+    gameContext.strokeStyle = "#003300";
+    gameContext.stroke();
   }
 }
 
-gameCanvas.addEventListener("mousedown", event => startGame(event));
-
-// copy
+drawGame();
+gameCanvas.addEventListener("mousedown", event => process(event));
